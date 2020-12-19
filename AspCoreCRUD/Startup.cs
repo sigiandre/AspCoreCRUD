63e12969abf8110e45b42e8d483feea36905bf0e
Andre Diaz <andrediaz@MacBook-Pro-de-Andre.local>
2020-12-18 23:14:53 -0500

    AspCoreCRUD API REST

---
+++ AspCoreCRUD/Startup.cs
@@ -0,0 +1,82 @@
+using System;
+using AspCoreCRUD.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace AspCoreCRUD
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "AspCoreCRUd API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("http://andrewprogramador.wixsite.com/cibernet"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Sigi Andre Diaz Quiroz",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/SigiAndreDiaz"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Enlace GitHub",
+                        Url = new Uri("https://github.com/sigiandre/AspCoreCRUD"),
+                    }
+                });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspCoreCRUD V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
